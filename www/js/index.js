276deb134df715ac40912cbe0531fe4439a103a3
Kaio <kaioreck@gmail.com>
2025-04-16 22:02:30 -0300

    Teste Commit

--- www/js/index.js
+++ www/js/index.js
@@ -15,7 +15,7 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- */
+ * OLA MANO/
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
